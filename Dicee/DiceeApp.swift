3ef97537480e4f67596569cfd1959769ed1eb98c
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-03-29 20:19:07 +0500

    Initial Commit

---
+++ Dicee/DiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp.swift
+//  Dicee
+//
+//  Created by Насрулло Исмоилжонов on 29/03/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
