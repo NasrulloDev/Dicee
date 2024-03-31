9eaceb63dcd97227ee307e1634468bca2246499c
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-03-31 14:21:42 +0500

    edit sheet
    new sheet() for editing the amount of dices

--- Dicee/ContentView.swift
+++ Dicee/ContentView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.dismiss) var dismiss
+    
     @State private var amountOfDice = 2
     
     @State private var total = 0
     @State private var previous = 0
     
     @State private var diceResults: [Int] = [1,1]
+    
+    @State private var showEdit = false
     
     var body: some View {
         VStack {
@@ -27,17 +31,43 @@ struct ContentView: View {
             
             Button("Roll dice"){
                 previous = total
-                diceResults = []
-                total = 0
-                for _ in 0..<amountOfDice {
-                    let diceResult = Int.random(in: 1..<7)
-                    diceResults.append(diceResult)
-                    total += diceResult
-                }
-                
+                rollDice()
+            }
+            
+            Button("Settings"){
+                showEdit = true
             }
         }
+        .sheet(isPresented: $showEdit) {
+            Form{
+                Section("How many dices do you want to roll?"){
+                    TextField("Amount...", value: $amountOfDice, format: .number)
+                }
+            }
+            Button("Apply"){
+                showEdit = false
+                restart()
+            }
+
+        }
         .padding()
+    }
+    
+    func rollDice() {
+        diceResults = []
+        total = 0
+        for _ in 0..<amountOfDice {
+            let diceResult = Int.random(in: 1..<7)
+            diceResults.append(diceResult)
+            total += diceResult
+        }
+    }
+    
+    func restart() {
+        rollDice()
+        total = diceResults.count
+        let newArray = diceResults.map{$0 * 0 + 1}
+        diceResults = newArray
     }
 }
 
